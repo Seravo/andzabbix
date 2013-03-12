f76f53b47a35d31377188e6f013fd94d02026456
m0rphex <sandrovb@gmail.com>
2013-03-12 17:08:56 +0200

    Added: logo and server select spinner(dd-menu)
    Todo: tweak spinner style, config button, MANY adjustments.
    This is an experimental stage so just getting confortable with
    everything!

--- gen/com/example/andzabbix/R.java
+++ gen/com/example/andzabbix/R.java
@@ -8,6 +8,11 @@
 package com.example.andzabbix;
 
 public final class R {
+    public static final class array {
+        /**  this is proof of concept, will be populated from user settings later 
+         */
+        public static final int select_server=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -29,29 +34,33 @@ public final class R {
         public static final int az_button_right_down=0x7f020005;
         public static final int az_button_right_up=0x7f020006;
         public static final int az_divider=0x7f020007;
-        public static final int background=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
+        public static final int az_logo=0x7f020008;
+        public static final int background=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int az_button_center=0x7f080003;
-        public static final int az_button_left_down=0x7f080004;
-        public static final int az_button_left_up=0x7f080001;
-        public static final int az_button_right_down=0x7f080005;
-        public static final int az_button_right_up=0x7f080002;
-        public static final int az_divider=0x7f080000;
-        public static final int az_divider2=0x7f080006;
+        public static final int action_settings=0x7f090009;
+        public static final int az_button_center=0x7f090005;
+        public static final int az_button_left_down=0x7f090006;
+        public static final int az_button_left_up=0x7f090003;
+        public static final int az_button_right_down=0x7f090007;
+        public static final int az_button_right_up=0x7f090004;
+        public static final int az_divider=0x7f090002;
+        public static final int az_divider2=0x7f090008;
+        public static final int az_logo=0x7f090000;
+        public static final int select_server=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int select_server=0x7f050003;
+        public static final int server_prompt=0x7f050002;
     }
     public static final class style {
         /** 
@@ -75,16 +84,17 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int az_button_center=0x7f060005;
-        public static final int az_button_left_down=0x7f060006;
-        public static final int az_button_left_up=0x7f060003;
-        public static final int az_button_right_down=0x7f060007;
-        public static final int az_button_right_up=0x7f060004;
-        public static final int az_divider=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int az_button_center=0x7f070006;
+        public static final int az_button_left_down=0x7f070007;
+        public static final int az_button_left_up=0x7f070004;
+        public static final int az_button_right_down=0x7f070008;
+        public static final int az_button_right_up=0x7f070005;
+        public static final int az_divider=0x7f070003;
+        public static final int select_server=0x7f070002;
     }
 }
